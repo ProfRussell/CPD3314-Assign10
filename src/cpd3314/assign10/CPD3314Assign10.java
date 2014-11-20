f59e0f5f00f287b6c71b69da72a6a841cd4db6b8
Len Payne <len.payne@lambtoncollege.ca>
2014-11-20 10:18:31 -0500

    Added Main Class / Controller

---
+++ src/cpd3314/assign10/CPD3314Assign10.java
@@ -0,0 +1,15 @@
+package cpd3314.assign10;
+
+/**
+ * This class is your main class, otherwise known as your Controller. Use it to:
+ * 1) Hold a reference to your data model (array of Day objects) 2) Modify and
+ * Retrieve Days 3) Spawn the Schedule JFrame
+ *
+ * @author <ENTER YOUR NAME HERE>
+ */
+public class CPD3314Assign10 {
+
+    public static void main(String[] args) {
+
+    }
+}
